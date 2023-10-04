7ec3f48402d06e76d556c4bc59bc575651aefa8f
adrianwb <adrian@sussol.net>
2023-10-04 15:38:12 +0100

    Make types public

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@
 pub mod berlinger;
 pub mod common;
 
-use crate::common::{
+pub use crate::common::{
     BreachType, Sensor, SensorType, TemperatureBreach, TemperatureBreachConfig, TemperatureLog,
 };
 
@@ -312,4 +312,26 @@ mod tests {
         }
     }
 
+}
+
+#[cfg(target_os="android")]
+#[allow(non_snake_case)]
+pub mod android {
+    extern crate jni;
+
+    use super::*;
+    use self::jni::JNIEnv;
+    use self::jni::objects::{JClass, JString};
+    use self::jni::sys::{jstring};
+
+    #[no_mangle]
+    pub unsafe extern fn Java_com_example_rusttest_RustGreetings_greeting(env: JNIEnv, _: JClass, java_pattern: JString) -> jstring {
+        // Our Java companion code might pass-in "world" as a string, hence the name.
+        let world = rust_greeting(env.get_string(java_pattern).expect("invalid pattern string").as_ptr());
+        // Retake pointer so that we can use it below and allow memory to be freed when it goes out of scope.
+        let world_ptr = CString::from_raw(world);
+        let output = env.new_string(world_ptr.to_str().unwrap()).expect("Couldn't create java string!");
+
+        output.into_inner()
+    }
 }
