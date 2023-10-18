f37b38936c1bcf7a69e550a83d168faa8b1140a4
adrianwb <adrian@sussol.net>
2023-10-18 11:43:58 +0100

    Process all USB files, filter only by start (3 days before last connected)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::error::Error;
 use std::env;
 use chrono::Duration;
-use rand::Rng;
 use std::fs::File;
 use std::io::Write;
 
@@ -10,27 +9,30 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let mut sensor: temperature_sensor::common::Sensor;
     let mut start_timestamp = None;
-    let mut end_timestamp =None; 
+    let mut sensor_serials: Vec<String> = Vec::new();
 
     if args.len() > 1 { // try specified file name
         sensor = temperature_sensor::read_sensor_file(args[1].trim())?;
-    } else { // pick a random file from USB
-        let sensor_serials = temperature_sensor::read_connected_serials()?;
-        let mut rng = rand::thread_rng();
-        let sensor_index: usize = rng.gen_range(0..sensor_serials.len());
-        sensor = temperature_sensor::read_sensor(&sensor_serials[sensor_index])?;
+        sensor_serials.push(sensor.serial);
+    } else { // read from USB
+        sensor_serials = temperature_sensor::read_connected_serials()?;
     }
 
-    if let Some(timestamp) = sensor.last_connected_timestamp {
-        start_timestamp = Some(timestamp - Duration::days(3)); // go back from 3 days
-        end_timestamp = Some(timestamp - Duration::hours(12)); // to 12 hours before
-    }
+    for sensor_serial in sensor_serials {
 
-    sensor = temperature_sensor::filter_sensor(sensor,start_timestamp, end_timestamp);
-    let output_path = "sensor_".to_owned() + &sensor.serial + "_filtered_output.txt";
-    if let Some(mut output) = File::create(&output_path).ok() {   
-        if write!(output, "{}", format!("{:?}\n\n", sensor)).is_ok() {
-            println!("Filtered output with start/end time {:?} - {:?} to: {}",start_timestamp,end_timestamp, &output_path);
+        sensor = temperature_sensor::read_sensor(&sensor_serial)?;
+
+        if let Some(timestamp) = sensor.last_connected_timestamp {
+            start_timestamp = Some(timestamp - Duration::days(3)); // go back from 3 days
+        }
+
+        sensor = temperature_sensor::filter_sensor(sensor, start_timestamp, None);
+        let output_path = "sensor_".to_owned() + &sensor.serial + "_filtered_output.txt";
+        
+        if let Some(mut output) = File::create(&output_path).ok() {   
+            if write!(output, "{}", format!("{:?}\n\n", sensor)).is_ok() {
+                println!("Filtered output with start time {:?} to: {}", start_timestamp, &output_path);
+            }
         }
     }
     
